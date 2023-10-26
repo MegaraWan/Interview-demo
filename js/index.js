e0cef94c57bae12c7a2bc021b68c4cd0a3893f37
MegaraWan <chaioyazhou@gmail.com>
2023-10-27 03:12:03 +0800

    banner 切完

---
+++ js/index.js
@@ -0,0 +1,34 @@
+//nav
+$(window).on("scroll", function () {
+  if ($(window).scrollTop() > $(".banner").height()) {
+    $(".nav").addClass("fixed");
+  } else {
+    $(".nav").removeClass("fixed");
+  }
+});
+
+
+
+//回到頂部的
+$(document).ready(function () {
+  // 捲軸偵測距離頂部超過 50 才顯示按鈕
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 50) {
+      if ($(".back-top").hasClass("hide")) {
+        $(".back-top").toggleClass("hide");
+      }
+    } else {
+      $(".back-top").addClass("hide");
+    }
+  });
+
+  // 點擊按鈕回頂部
+  $(".back-top").on("click", function (event) {
+    $("html, body").animate(
+      {
+        scrollTop: 0
+      },
+      500 // 回頂部時間為 500 毫秒
+    );
+  });
+});
