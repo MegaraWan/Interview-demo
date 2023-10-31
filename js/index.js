a9c2ddedf1c6a9848caaa1d5909fd1843356289a
Megara Wan <>
2023-10-31 22:13:03 +0800

    剩輪播

--- js/index.js
+++ js/index.js
@@ -16,6 +16,70 @@ $(window).on("scroll", function () {
 });
 
 
+jQuery(document).ready(function() {
+  console.log("Document is ready!");
+
+  function checkSectionHighlight() {
+      $('li a').each(function() {
+          var currLink = $(this);
+          var refElement = $(currLink.attr('href'));
+          console.log("Checking link to: ", currLink.attr('href'));
+
+          if (refElement.length) {
+              if (refElement.position().top <= $(window).scrollTop() && refElement.position().top + refElement.height() > $(window).scrollTop()) {
+                  console.log("Adding arrive to: ", currLink.attr('href'));
+                  $('li a').removeClass('arrive');  
+                  currLink.addClass('arrive');  
+              }
+          } else {
+              console.log("Didn't find element for: ", currLink.attr('href'));
+          }
+      });
+  }
+
+  $(window).on('scroll', function() {
+      console.log("Scrolled");
+      checkSectionHighlight();
+  });
+
+  $('li a').on('click', function(e) {
+      e.preventDefault();
+      var target = $(this).attr('href');
+      
+      if (target && $(target).length) {
+          $('html, body').animate({
+              scrollTop: $(target).offset().top
+          }, 1000, checkSectionHighlight); 
+      }
+  });
+
+  // 初始检查
+  checkSectionHighlight();
+});
+
+
+//input 點選改說明字體顏色
+document.addEventListener('DOMContentLoaded', function() {
+  let inputs = document.querySelectorAll('.member-info, .member-needs');
+
+  inputs.forEach(function(input) {
+      input.addEventListener('focus', function() {
+          let spanElement = this.previousElementSibling;
+          if (spanElement && spanElement.classList.contains('contact-info-input-title')) {
+              spanElement.style.color = '#f15a24'; 
+          }
+      });
+
+      input.addEventListener('blur', function() {
+          let spanElement = this.previousElementSibling;
+          if (spanElement && spanElement.classList.contains('contact-info-input-title')) {
+              spanElement.style.color = '';  
+          }
+      });
+  });
+});
+
+
 //回到頂部的
 $(document).ready(function () {
   // 捲軸偵測距離頂部超過 50 才顯示按鈕
