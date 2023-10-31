a014121a4ffba5c528972fbaf68c061bc0083c21
Megara Wan <>
2023-10-31 23:45:55 +0800

    套用輪播套件

--- js/index.js
+++ js/index.js
@@ -53,9 +53,31 @@ jQuery(document).ready(function() {
       }
   });
 
-  // 初始检查
   checkSectionHighlight();
 });
+
+
+
+var swiper = new Swiper('.swiper', {
+  slidesPerView: 3,
+  direction: getDirection(),
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  on: {
+    resize: function () {
+      swiper.changeDirection(getDirection());
+    },
+  },
+});
+
+function getDirection() {
+  var windowWidth = window.innerWidth;
+  var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+  return direction;
+}
 
 
 //input 點選改說明字體顏色
